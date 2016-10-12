ff42e81cbf57e5937f9b877b70cc26bb47c45db9
Andriy Oblivantsev <eslider@gmail.com>
2016-10-12 15:50:15 +0200

    Add module bundle class

---
+++ MapbenderStyleManagerBundle.php
@@ -0,0 +1,20 @@
+<?php
+namespace Mapbender\StyleManagerBundle;
+
+use Mapbender\CoreBundle\Component\MapbenderBundle;
+
+/**
+ * Class MapbenderStyleManagerBundle
+ *
+ * @author  Andriy Oblivantsev <eslider@gmail.com>
+ */
+class MapbenderStyleManagerBundle extends MapbenderBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public function getElements()
+    {
+        return array();
+    }
+}
